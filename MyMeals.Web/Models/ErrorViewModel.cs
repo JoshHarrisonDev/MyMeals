980ee42a025f5efc52d2e45728eb219471c8bfcb
JoshHarrisonDev <joshharrison@hotmail.co.uk>
2022-07-10 06:26:08 +0100

    Add project files.

---
+++ MyMeals.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyMeals.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
